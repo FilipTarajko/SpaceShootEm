160fee98199bb1a89c27f0284eb02a3b143c3f55
Filip Tarajko <filiptarajko@gmail.com>
2021-02-24 17:50:38 +0100

    change waveDisplay

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour
     public Player player;
     public BasicEnemy basicEnemy;
     public GameObject dynamic;
+    public Transform enemiesParent;
     public int wave;
     public TMP_Text waveDisplay;
 
@@ -21,10 +22,52 @@ public class GameController : MonoBehaviour
     {
         for (;;)
         {
-            wave += 1;
-            UpdateWaveText();
-            StartCoroutine(SpawnWave(wave));
-            yield return new WaitForSeconds(5);
+            if(enemiesParent.childCount == 0)
+            {
+                wave += 1;
+                UpdateWaveText();
+                StartCoroutine(WaveDisplaySize());
+                StartCoroutine(WaveDisplayColor());
+                yield return new WaitForSeconds(2.5f);
+                StartCoroutine(SpawnWave(wave));
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator WaveDisplayColor()
+    {
+        Color waveDisplayColor = waveDisplay.color;
+        float alpha = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            alpha += 0.01f;
+            waveDisplayColor = ChangeAlpha(waveDisplayColor, alpha);
+            waveDisplay.color = waveDisplayColor;
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < 100; i++)
+        {
+            alpha -= 0.01f;
+            waveDisplayColor = ChangeAlpha(waveDisplayColor, alpha);
+            waveDisplay.color = waveDisplayColor;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private Color ChangeAlpha(Color color, float value)
+    {
+        return new Color(color.r, color.g, color.b, value);
+    }
+
+    IEnumerator WaveDisplaySize()
+    {
+        waveDisplay.fontSize = 200f;
+        for (int i = 0; i < 250; i++)
+        {
+            waveDisplay.fontSize += 0.5f;
+            yield return new WaitForSeconds(0.01f);
         }
     }
 
@@ -42,7 +85,7 @@ public class GameController : MonoBehaviour
             float horizontalMargin = 50;
             float spawnX = Random.Range(-Screen.width/2+horizontalMargin, Screen.width/2-horizontalMargin);
             float spawnY = Screen.height*(0.5f+data.entityBorder);
-            BasicEnemy spawnedEnemy = Instantiate(basicEnemy, new Vector3(spawnX,spawnY,0), Quaternion.Euler(0,0,0), dynamic.transform);
+            BasicEnemy spawnedEnemy = Instantiate(basicEnemy, new Vector3(spawnX,spawnY,0), Quaternion.Euler(0,0,0), enemiesParent);
             spawnedEnemy.health = 1;
             spawnedEnemy.speed = wave*100+1500;
             spawnedEnemy.damage = 1;
